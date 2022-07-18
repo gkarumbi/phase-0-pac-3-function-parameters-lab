84069240b11d5209d983d531256d78459ce80b64
George Karumbi <gkarumbi@gmail.com>
2022-07-18 20:58:49 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function logTwoValues(value1,value2){
+    console.log(`The two values are ${value1} and ${value2}.`);
+}
+
+function sayHelloTo(firstName){
+    console.log(`Hello, ${firstName}!`);
+}
+
+function sayHelloTo(firstName = "User"){
+    console.log(`Herroooom, ${firstName} ni msarendo!!`);
+}
+
+sayHelloTo("Raira");
